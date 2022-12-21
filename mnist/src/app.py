e428bd76e57c4790ddb48053210be5196be073ee
davidfox <foxy1987@gmail.com>
2022-12-20 21:27:36 -0700

    fixed mnist training source code

--- mnist/src/app.py
+++ mnist/src/app.py
@@ -12,11 +12,11 @@ import logging
 import sys
 
 
-# Use this format (%Y-%m-%dT%H:%M:%SZ) to record timestamp of the metrics
-# logging.basicConfig(
-#     format="%(asctime)s %(levelname)-8s %(message)s",
-#     datefmt="%Y-%m-%dT%H:%M:%SZ",
-#     level=logging.DEBUG)
+#Use this format (%Y-%m-%dT%H:%M:%SZ) to record timestamp of the metrics
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%SZ",
+    level=logging.DEBUG)
 
 
 def parse_arguments(argv):
@@ -86,17 +86,13 @@ def build_and_compile_cnn_model(dropout, lr):
 
 def make_dataset():
   num_classes = 10
-  # Scaling MNIST data from (0, 255] to (0., 1.]
-
-
-  input_shape = (28, 28, 1)
 
   # Load the data and split it between train and test sets
   (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
   # Scale images to the [0, 1] range
-  x_train = x_train.astype("float32") / 255
-  x_test = x_test.astype("float32") / 255
+  x_train = x_train.astype("float32") / 255.
+  x_test = x_test.astype("float32") / 255.
   # Make sure images have shape (28, 28, 1)
   x_train = np.expand_dims(x_train, -1)
   x_test = np.expand_dims(x_test, -1)
@@ -112,13 +108,15 @@ def main(argv=None):
   model = build_and_compile_cnn_model(dropout=0.5, lr=args.learning_rate)
 
   logdir = "/logs" + datetime.now().strftime("%Y%m%d-%H%M%S")
-  tensorboard_callback= tf.keras.callbacks.TensorBoard(log_dir=logdir)
+  tensorboard_callback= tf.keras.callbacks.TensorBoard(log_dir=logdir,
+                                                       update_freq='epoch',
+                                                    )
     
   std_out = StdOutCallback()
   model.fit(x=x_train, 
           y=y_train, 
-          epochs=2,
-          batch_size=128, 
+          epochs=5,
+          batch_size=args.batch_size, 
           validation_data=(x_test, y_test), 
           callbacks=[tensorboard_callback, std_out])
 
