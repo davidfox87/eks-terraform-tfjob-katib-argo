53526c32770898ad82b10008b5ac8b87e4302a21
davidfox <foxy1987@gmail.com>
2023-01-12 22:22:11 -0700

    added aws back for kubernetes and helm providers

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -10,47 +10,40 @@ provider "aws" {
   }
 }
 
-# provider "kubernetes" {
-#   host                   = module.my-eks.cluster_endpoint
-#   cluster_ca_certificate = "${base64decode(module.my-eks.kubeconfig-certificate-authority-data)}"
-
-#   exec {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       args        = ["eks", "get-token", "--cluster-name", var.cluster-name]
-#       command     = "aws"
-#   }
-# }
-
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.my-eks.cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.my-eks.kubeconfig-certificate-authority-data)
-
-#     exec {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       command     = "aws"
-#       # This requires the awscli to be installed locally where Terraform is executed
-#       args = ["eks", "get-token", "--cluster-name", var.cluster-name]
-#     }
-#   }
-# }
-
-
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "minikube"
+  host                   = module.my-eks.cluster_endpoint
+  cluster_ca_certificate = "${base64decode(module.my-eks.kubeconfig-certificate-authority-data)}"
+
+  exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster-name]
+      command     = "aws"
+  }
 }
 
 provider "helm" {
- kubernetes {
-  config_path    = "~/.kube/config"
-  config_context = "minikube"
- }
-}
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.my-eks.cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.my-eks.kubeconfig-certificate-authority-data)
+  kubernetes {
+    host                   = module.my-eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.my-eks.kubeconfig-certificate-authority-data)
 
-#   }
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", var.cluster-name]
+    }
+  }
+}
+
+
+# provider "kubernetes" {
+#   config_path    = "~/.kube/config"
+#   config_context = "minikube"
+# }
+
+# provider "helm" {
+#  kubernetes {
+#   config_path    = "~/.kube/config"
+#   config_context = "minikube"
+#  }
 # }
